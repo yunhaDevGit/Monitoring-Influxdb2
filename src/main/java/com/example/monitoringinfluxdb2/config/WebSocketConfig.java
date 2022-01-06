5f47d22ec3f56e0a62a3235570b9ace8419132a6
yunhaDevGit <yunhaDev98@gmail.com>
2022-01-06 19:07:29 +0900

    feat: client가 websocket을 connect 또는 disconnect 할 때 호출되는 handler

--- src/main/java/com/example/monitoringinfluxdb2/config/WebSocketConfig.java
+++ src/main/java/com/example/monitoringinfluxdb2/config/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package com.example.monitoringinfluxdb2.config;
 
+import com.example.monitoringinfluxdb2.websocket.StompHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +12,9 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+  @Autowired
+  private StompHandler stompHandler;
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -20,5 +26,10 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/ws");
     registry.addEndpoint("/ws").withSockJS();
+  }
+
+  @Override
+  public void configureClientInboundChannel(ChannelRegistration registration) {
+    registration.interceptors(stompHandler);
   }
 }
